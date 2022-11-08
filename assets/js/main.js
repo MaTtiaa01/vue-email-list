03305ad479e5cb1692868db576385dedb366fe77
MaTtiaa01 <mattia2001@live.it>
2022-11-08 14:53:12 +0100

    print in console all the email

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,10 +14,19 @@ createApp({
         }
     },
     methods : {
-        
+        getEmail(){
+            for (let index = 0; index < 10; index++) { 
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then(response =>{
+                        console.log(response.data.response);
+                    })
+                
+            }
+        }
     },
     mounted(){
-
+        this.getEmail()
     }
 
 }).mount("#app")
